bd48610a53b002b4a4ba51ff53761e228b7c2ff8
Brian Soltis <brian@MBP.PK5001Z>
2017-04-04 21:23:24 -0500

    Started adding comments for program

---
+++ js/classic-script.js
@@ -0,0 +1,6 @@
+// Nothing works until power switch is on
+    // LED display lights up when power switch is on
+    // "Strict" LED indicator won't display until power switch is on
+// "Start" button starts game
+    // Randomizes a game move and stores it in a global array
+    // Number of moves is displayed in digital readout
